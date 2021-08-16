f59a2ac5c6e4cf3a378a1b7dd286c60f6cd3f15f
Dominik Rumian <xrumian@fi.muni.cz>
2021-08-16 23:39:48 +0200

    creating UI

--- graph-visualization/src/components/graphSection/GraphSection.js
+++ graph-visualization/src/components/graphSection/GraphSection.js
@@ -27,12 +27,14 @@ export const GraphSection = (props) => {
         },
     };
 
+    // TODO: Create pseudocode components (probably using 1 with switch statement and using props to show correct code)
     return(
         <div {...classes()}>
             <div {...classes('graph')}>
                 <Graph graph={graph} options={options} />
             </div>
-            <SparseGraphSubprocedure procedure={1} step={0} />
+            <SparseGraphSubprocedure {...classes('main-procedure')} procedure={2} step={0} />
+            <SparseGraphSubprocedure {...classes('sub-procedure')} procedure={3} step={0} />
         </div>
     )
 }
--- graph-visualization/src/components/mainView/MainView.js
+++ graph-visualization/src/components/mainView/MainView.js
@@ -1,9 +1,16 @@
+import './MainView.css'
+
 import {TopPanel} from "../topPanel/TopPanel";
 import {GraphSection} from "../graphSection/GraphSection";
+import BEMHelper from 'react-bem-helper';
+
+const classes = new BEMHelper({
+    name: 'main-view',
+});
 
 export const MainView = (props) => {
     return(
-        <div>
+        <div {...classes()}>
             <TopPanel />
             <GraphSection />
         </div>
